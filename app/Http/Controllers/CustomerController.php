bda6b2826750e0111700a18eefc1b7f93c8334e8
Jesper Dybdahl Hede <jdh@novataris.com>
2018-04-03 10:53:48 +0200

    Price and date calculated

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Agreement;
 use App\Customer;
+use App\Delivery;
 use App\Invoice;
+
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\View;
+
+use \DateTime;
 
 class CustomerController extends Controller
 {
@@ -25,19 +30,36 @@ class CustomerController extends Controller
     {
         $customer = Customer::findOrFail($id);
 
-        // //'agreement_id', 'invoice_no', 'invoice_due_at', 'amount'
+        $agreement = Agreement::where('type', 'monthly')->first();
+        $deliveries = Delivery::where('customer_id', $id)->get();
+
+        // Object to insert.
         $invoice = new Invoice;
+
+        //Calculate price
+        $price = 0;
+
+        foreach($deliveries as $delivery){
+            $price += $delivery->count * $agreement->unit_price;            
+        }        
+
+ 
+        // //'agreement_id', 'invoice_no', 'invoice_due_at', 'amount'
+        
 
         // // Get invoice count
 
         $invoice->agreement_id = 100;
         $invoice->invoice_no = 100;
-        $invoice->invoice_due_at = date(DATE_RFC2822);
-        $invoice->amount = 1000000; // One million!
+
+        // Set due date 30 days ahead.
+        $date = new DateTime();
+        $invoice->invoice_due_at = $date->modify('+1 day');
+        $invoice->amount = $price; 
 
         $invoice->save();
         
         
-        return Redirect::action('CustomerController@show',['id' => $id]);
+        // return Redirect::action('CustomerController@show',['id' => $id]);
     }
 }
